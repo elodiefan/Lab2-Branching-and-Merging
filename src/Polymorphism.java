62d047c07f046ad9e33bfbe244563f25e5ce6195
Elodie Fan <elodie.fan25@gmail.com>
2026-05-14 19:42:52 -0400

    commited Polymorphism.java onto branch task_1

--- src/Polymorphism.java
+++ src/Polymorphism.java
@@ -1,5 +1,5 @@
 public class Polymorphism {
-    // TODO TASK 1: Fix the bugs in this code to pass PolymorphismTest.java
+    // old_TODO TASK 1: Fix the bugs in this code to pass PolymorphismTest.java
 
     /**
      * Returns the role.
@@ -7,13 +7,13 @@ public class Polymorphism {
      * @return the role of the person
      */
     public static String getStudentRole(Person p) {
-        // TODO: right now for student, it is not returning the role to be student.
-        return p.role;
+        // old_TODO: right now for student, it is not returning the role to be student.
+        return p.getRole();
     }
 }
 
 class Person {
-    public final String role = "General";
+    // public final String role = "General";
     private final String name;
 
     public Person(String name) {
@@ -23,14 +23,22 @@ class Person {
     public String getName() {
         return this.name;
     }
+
+    public String getRole() { return "General";
+    }
 }
 
 class Student extends Person {
-    public final String role = "Student";
+    // public final String role = "Student";
     private final String studentId;
 
     public Student(String name, String studentId) {
         super(name);
         this.studentId = studentId;
+    }
+
+    @Override
+    public String getRole() {
+        return "Student";
     }
 }
